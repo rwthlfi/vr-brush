43840e90b67a4a8b38ec1734dd11e33a2a4b31af
Xysaya <Astrid.w@rkentin.de>
2023-12-13 13:12:32 +0100

    Redesigned the drawing system to use lineRenderers
    Using the script LineSmoother from EasyCurvedLine (github) to make them 3D.

--- Assets/Drawing System/Stroke.cs
+++ Assets/Drawing System/Stroke.cs
@@ -1,33 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
 public class Stroke : MonoBehaviour
 {
-
+    public LineRenderer lineRenderer;
     public List<Vector3> segments;
-    public GameObject brushShape;
 
     private void Awake()
     {
         segments = new List<Vector3>();
+        this.lineRenderer = gameObject.AddComponent<LineRenderer>();
     }
 
     public void AddPoint(Vector3 nextPoint)
     {
-
-        Vector3 temp = nextPoint - segments.Last();
-
-        float rotationZ = Mathf.Rad2Deg * Mathf.Asin(temp.y / Mathf.Sqrt((temp.x * temp.x) + (temp.y * temp.y)));
-        float rotationX = Mathf.Rad2Deg * Mathf.Asin(temp.z / Mathf.Sqrt((temp.y * temp.y) + (temp.z * temp.z)));
-
         segments.Add(nextPoint);
-
-        Instantiate(brushShape, nextPoint, Quaternion.Euler(rotationX, 0, rotationZ));
-    }
-    public void FirstPoint(Vector3 nextPoint)
-    {
-        segments.Add(nextPoint);
+        Vector3[] smoothedPoints = EasyCurvedLine.LineSmoother.SmoothLine(segments.ToArray(), 0.15f);
+        lineRenderer.positionCount = smoothedPoints.Length;
+        lineRenderer.SetPositions(smoothedPoints);
     }
 }
