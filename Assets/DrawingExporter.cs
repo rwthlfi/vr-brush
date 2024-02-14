dbfc4f7d4b669a08a9c249f187c2878d6cc62772
Helena Rickmann <hrickmann@hotmail.com>
2024-02-14 12:16:00 +0100

    basic scripts for converting strokes to json and handling android permissions

---
+++ Assets/DrawingExporter.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEngine;
+
+public class DrawingExporter : MonoBehaviour
+{
+    private AndroidPermissionHandler _permissionHandler;
+    private GameObject[] _strokes;
+    private string _path = Application.persistentDataPath;
+    
+
+    private void Start()
+    { // in Brush.cs newStroke.tag = "Stroke" 
+        _permissionHandler = new AndroidPermissionHandler();
+        if (_permissionHandler.CheckStoragePermissions())
+        {
+            ObjectsToJSON();
+        } else
+        {
+            Debug.Log("Permission for accessing storage not granted. Unable to save drawing");
+        }
+    }
+
+
+    private void ObjectsToJSON()
+    {
+        _strokes = GameObject.FindGameObjectsWithTag("Stroke");
+        var sb = new StringBuilder();
+
+        foreach (GameObject stroke in _strokes)
+        {
+            string json = JsonUtility.ToJson(stroke);
+            sb.AppendLine(json);
+        }
+
+        File.WriteAllText(FilePath(), sb.ToString());
+    }
+    
+    private string FilePath()
+    {
+        return _path + "/drawing" + System.DateTime.Now.ToString("yyyyMMddHHmm") + ".json";
+    }
+
+    
+}
